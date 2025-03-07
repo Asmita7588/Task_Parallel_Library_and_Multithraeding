696f75af9fd1df4c663c2534fc0f0d81740170dc
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-03-07 16:32:20 +0530

    implements  Task for diffrent diffrent operation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static async Task Main(string[] args)
     {
@@ -20,11 +22,65 @@
 
         //Tpl
 
-        Task task1 =Task.Run(() => PrintNumbers());
-        Task task2 =Task.Run(() => PrintNumbers());
+        Task task1 = Task.Run(() => PrintNumbers());
+        Task task2 = Task.Run(() => PrintNumbers());
 
         await Task.WhenAll(task1, task2);
 
+        //Task Creation
+        Task<bool> taskbool = Task.Run(() => {
+            return true;
+        });
+
+        bool result = taskbool.Result;
+
+        //task that return int value
+
+        Task<int> taskint = Task.Run(() => 42);
+
+        //task that continues with previous task
+
+        Task<string> taskstring = taskint.ContinueWith(prviousTask =>
+        {
+           int result = prviousTask.Result;
+            return $"previous task return value is {result}";
+        });
+
+        //create and start task in one operation
+
+       Task result1 = Task.Factory.StartNew(() => DoComputation(1.0));
+
+        Console.WriteLine($"result sum = {result1.ToString()}");
+
+        //detached child task 
+
+        var Outer = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("Outer task beginnning");
+
+            var Child = Task.Factory.StartNew(() => { 
+
+                Thread.SpinWait(5000000);
+                Console.WriteLine("Detached task is completed");
+            });
+
+            
+        });
+
+        Outer.Wait();
+        Console.WriteLine("Outer task is completed");
+   
+
+
+    }
+
+    public static double DoComputation(Double start) { 
+
+         double sum = 0;
+        for (var i = 1; i <= start + 10; i += 1) { 
+            sum += i;
+        }
+        return sum;
     }
     public static void PrintNumbers()
     {
